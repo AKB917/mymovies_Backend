5979b8ba14c94b50d16350f16fc4af721fcf009d
Your name <Your email>
2025-02-05 15:53:15 +0100

    deployement

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const fetch = require('node-fetch');
 
 const TDM_API_KEY = process.env.TDM_API_KEY;
 
-
+//Import movies from API
 router.get('/movies', (req, res) =>{
     fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TDM_API_KEY}`)
     .then(response => response.json())
