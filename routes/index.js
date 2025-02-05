2e16ad647fba04a97f9333b98ea029c8fb9fda7e
Your name <Your email>
2025-02-05 15:36:40 +0100

    deployement

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+const fetch = require('node-fetch');
+
+const TDM_API_KEY = process.env.TDM_API_KEY;
+
+
+router.get('/movies', (req, res) =>{
+    fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${TDM_API_KEY}`)
+    .then(response => response.json())
+    .then(data => {
+        res.json({movie:data.results });
+    })
+    
+})
+
+module.exports = router;
